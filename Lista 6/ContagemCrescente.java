42274260bbc21a1032d44025c14049fc390d9001
Fabricio <fabriciotecti@gmail.com>
2023-08-16 00:19:05 -0300

    Lista de exercicios de logica

---
+++ Lista 6/ContagemCrescente.java
@@ -0,0 +1,16 @@
+
+
+public class ContagemCrescente {
+	
+	public static void main (String[] args) {
+		
+		int in = 0;
+		
+		while(in<=10)
+		{
+			System.out.print(in +" ");
+			in++;
+		}
+	}
+}
+
---
+++ Lista 6/OrdemDecrescente.java
@@ -0,0 +1,16 @@
+
+public class OrdemDecrescente {
+	
+	public static void main (String[] args) {
+		
+		int ini = 10;
+		
+		while(ini>0)
+		{
+			System.out.print(ini+ " ");
+			ini--;
+		}
+		
+	}
+}
+
