9e9cb22588c2ea3e2fbbe5d3f3fb7b7dc568c4a2
Fabricio <fabriciotecti@gmail.com>
2023-08-16 00:21:54 -0300

    Lista de exercicios de logica

---
+++ Lista 1/OiTchau.java
@@ -0,0 +1,13 @@
+public class OiTchau {
+	
+	public static void main (String[] args) {
+		class Oi {
+		System.out.print("Oi");
+				}
+		class Tchau {
+		System.out.print("Tchau");
+					}
+}
+}
+
+
